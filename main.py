8bd62c2d69b6ff8d9940a5131ed1e8700cec0e6e
JashanPuri <jashan2019@iiitkottayam.ac.in>
2020-10-16 09:46:47 +0530

    Flask Setup and routes added

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to our price tracking app's api"
+
+
+@app.route('/api/flipkart')
+def flipkart_api():
+    return 'Hello Nishant'
+
+
+@app.route('/api/amazon')
+def amazon_api():
+    return 'Hello Chandrima'
+
+
+@app.route('/api/croma')
+def croma_api():
+    return 'Hello Charles'
+
+
+if __name__ == '__main__':
+    app.run()
