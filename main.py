2cc50ef7066fc1078cad06210646023b1b32e8d2
charles-vj <charles2019@iiitkottayam.ac.in>
2020-10-17 09:19:37 +0530

    ':)'

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
@@ -19,10 +21,32 @@ def amazon_api():
     return 'Hello Chandrima'
 
 
-@app.route('/api/croma')
+@app.route('/api/croma',methods=['GET'])
 def croma_api():
-    print("Mujhe last kyu rakha :(")
-    return 'Hello Charles'
+    if request.method == 'GET':
+        u = 'https://www.croma.com/'
+        text = str(request.args['text'])
+        print(text)
+        if " " in text:
+            text = str(text).replace(" ", "%20")
+        else:
+            pass
+        search = '/search_results?q=' + text
+        finalurl = u + search
+        print(finalurl)
+        body = requests.get(finalurl).content
+        scrap = BeautifulSoup(body, 'html.parser')
+        links = scrap.find_all('a', {'class': 'product-title'})
+        l = []
+        for i in links:
+            d = {}
+            p_url = u + i.get('href')
+            p_content = requests.get(p_url).content
+            p_soup = BeautifulSoup(p_content, 'html.parser')
+            d['p'] = p_soup.find('h1', {'class': 'pd-title'}).text
+            d['price'] = p_soup.find('span', {'class': 'amount'}).text
+            l.append(d)
+    return jsonify(l)
 
 
 if __name__ == '__main__':
