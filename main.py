cbd13831ac65e3acd910518cd8bb738145be7392
Nishant2125 <nishantkmr264@gmail.com>
2020-10-20 11:47:04 +0530

    comment added.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def home():
 
 @app.route('/api/flipkart')
 def flipkart_api():
+    # print("i m nishant").
     if request.method == 'GET':
         u = 'https://www.flipkart.com'
         # converting the request in query to string and storing it.
@@ -32,9 +33,9 @@ def flipkart_api():
         l = []
         for i in links:
             d = {}
-            p_url = u + i.get('href')                           #finding and adding all the links to the products into the original link.
-            p_content = requests.get(p_url).content              #getting source code of the product page.
-            p_soup = BeautifulSoup(p_content, 'html.parser')     #parsing all data within div tag having class : sp grid.
+            p_url = u + i.get('href')                           # finding and adding all the links to the products into the original link.
+            p_content = requests.get(p_url).content              # getting source code of the product page.
+            p_soup = BeautifulSoup(p_content, 'html.parser')     # parsing all data within div tag having class : sp grid.
             d['title'] = p_soup.find('span', {'class': '_35KyD6'}).text
             d['price'] = p_soup.find('div', {'class': '_1vC4OE _3qQ9m1'}).text
             d['link'] = p_url
