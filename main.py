efc1e22bd4cfead0e6cfcb8b9f04ad15ee156cd2
JashanPuri <jashan2019@iiitkottayam.ac.in>
2020-10-20 09:30:02 +0530

    nothing much

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def flipkart_api():
             p_soup = BeautifulSoup(p_content, 'html.parser')
             d['title'] = p_soup.find('span', {'class': '_35KyD6'}).text
             d['price'] = p_soup.find('div', {'class': '_1vC4OE _3qQ9m1'}).text
-
             d['link'] = p_url
             l.append(d)
     return jsonify({'flipkart': l})
@@ -106,7 +105,7 @@ def reliance_api():
             p_soup = BeautifulSoup(p_content, 'html.parser')
             d['title'] = p_soup.find('div', {'class': 'pdp__title'}).text
             d['price'] = p_soup.find('span', {'class': 'pdp__offerPrice'}).text
-            d['price'] = d['price'].replace("\u20b9", "")
+            # d['price'] = d['price'].replace("\u20b9", "")
             d['link'] = p_url
 
             l.append(d)
